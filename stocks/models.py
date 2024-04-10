e7ca23d8dd840153eda99d781e7e86a05952f1a4
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-04-10 20:16:45 +0000

    Create stocks models and add serializers file.

---
+++ stocks/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Stock(models.Model):
+    order_choices = [
+        ('buy', 'Buy'),
+        ('sell', 'Sell'),
+        ('hold', 'Hold')
+    ]
+    sector_choices = [
+        ('', '---------'),
+        ('information technology', 'Information Technology'),
+        ('health', 'Health Care'),
+        ('financials', 'Financials'),
+        ('consumer discretionary', 'Consumer Discretionary'),
+        ('communication services', 'Communication Services'),
+        ('industrials', 'Industrials'),
+        ('consumer staples', 'Consumer Staples'),
+        ('energy', 'Energy'),
+        ('utilities', 'Utilities'),
+        ('real estate', 'Real Estate'),
+        ('materials', 'Materials')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=120)
+    symbol = models.CharField(max_length=10)
+    company_name = models.CharField(max_length=100)
+    sector = models.CharField(
+        max_length=30, choices=sector_choices, default=''
+    )
+    order = models.CharField(
+        max_length=10, choices=order_choices, default='hold'
+    )
+    order_date = models.DateField(blank=True)
+    order_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    quantity = models.PositiveIntegerField(blank=True)
+    content = models.TextField()
+    chart = models.ImageField(
+        upload_to='images/', blank=True
+    )
+    image = models.ImageField(
+        upload_to='images/', blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.image:
+            if self.order == 'buy':
+                self.image = '../bull_mkjux3'
+            elif self.order == 'sell':
+                self.image = '../bear_apzdgz'
+            else:
+                self.image = '../hold_fx1ysi'
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
---
+++ stocks/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from posts.models import Stock
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Stock
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'symbol', 'company_name', 'sector',
+            'order', 'order_date', 'rder_price', 'quantity',
+            'content', 'chart', 'image'
+        ]
