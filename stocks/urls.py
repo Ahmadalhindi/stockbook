5e2c235c139c4a48493089a8bcb2aab0567d2f31
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-04-12 16:39:07 +0000

    Add get method in the new class stock detail, so we can retrive the stock post by id.

--- stocks/urls.py
+++ stocks/urls.py
@@ -3,4 +3,5 @@ from stocks import views
 
 urlpatterns = [
     path('stocks/', views.StockList.as_view()),
+    path('stocks/<int:pk>/', views.StockDetail.as_view())
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Stock
 from .serializers import StockSerializer
+from stock_book.permissions import IsOwnerOrReadOnly
 
 
 class StockList(APIView):
@@ -30,3 +32,22 @@ class StockList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+class StockDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = StockSerializer
+
+    def get_object(self, pk):
+        try:
+            stock = Stock.objects.get(pk=pk)
+            self.check_object_permissions(self.request, stock)
+            return stock
+        except Stock.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        stock = self.get_object(pk)
+        serializer = StockSerializer(
+            stock, context={'request': request}
+        )
+        return Response(serializer.data)
