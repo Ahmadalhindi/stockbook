17159a765e274ce2919450bcf597366ec4543708
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-04-14 06:59:39 +0000

    Finish working on followers resource by adding FollowerDetail class in views to get and delete a follower by using gereric views to retrive and destroy.

--- followers/views.py
+++ followers/views.py
@@ -5,15 +5,15 @@ from .serializers import FollowerSerializer
 
 
 class FollowerList(generics.ListCreateAPIView):
-    """
-    List all followers, i.e. all instances of a user
-    following another user'.
-    Create a follower, i.e. follow a user if logged in.
-    Perform_create: associate the current logged in user with a follower.
-    """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
