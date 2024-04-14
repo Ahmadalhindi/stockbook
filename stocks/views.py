e7b743300fc5078f71dfd08c01b5bc4d9e1d369e
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-04-14 07:24:08 +0000

    Refactoring Post and Profile Views by using generic views.

--- stocks/views.py
+++ stocks/views.py
@@ -1,73 +1,19 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from stock_book.permissions import IsOwnerOrReadOnly
 from .models import Stock
 from .serializers import StockSerializer
-from stock_book.permissions import IsOwnerOrReadOnly
 
 
-class StockList(APIView):
+class StockList(generics.ListCreateAPIView):
     serializer_class = StockSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
-    
-    def get(self, request):
-        stocks = Stock.objects.all()
-        serializer = StockSerializer(
-            stocks, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = StockSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Stock.objects.all()
 
-class StockDetail(APIView):
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class StockDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = StockSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = StockSerializer
-
-    def get_object(self, pk):
-        try:
-            stock = Stock.objects.get(pk=pk)
-            self.check_object_permissions(self.request, stock)
-            return stock
-        except Stock.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        stock = self.get_object(pk)
-        serializer = StockSerializer(
-            stock, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        stock = self.get_object(pk)
-        serializer = StockSerializer(
-            stock, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        stock = self.get_object(pk)
-        stock.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
-
+    queryset = Stock.objects.all()
