424153f08900e23401d2a1cf44a35137bc9b4cb9
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-04-26 08:05:09 +0000

    Create the main Post component and add bull and comment icons to be displayed.

---
+++ frontend/src/pages/stocks/Stock.js
@@ -0,0 +1,89 @@
+import React from "react";
+import styles from "../../styles/Stock.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+
+const Stock = (props) => {
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    comments_count,
+    bulls_count,
+    bull_id,
+    title,
+    content,
+    company_name,
+    symbol,
+    sector,
+    order,
+    order_date,
+    order_price,
+    quantity,
+    image,
+    updated_at,
+    stockPage,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && stockPage && "..."}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`/stocks/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+        <div className={styles.PostBar}>
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own stock post!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : bull_id ? (
+            <span onClick={() => {}}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={() => {}}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like posts!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
+          {bulls_count}
+          <Link to={`/stocks/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+          {comments_count}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Stock;
--- frontend/src/pages/stocks/StockPage.js
+++ frontend/src/pages/stocks/StockPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Stock from "./Stock";
 
 function StockPage() {
   const { id } = useParams();
@@ -32,7 +33,7 @@ function StockPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Stock component</p>
+        <Stock {...stock.results[0]} setStocks={setStock} stockPage />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
