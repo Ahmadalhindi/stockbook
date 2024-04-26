5b94581376f138e10b86e02788cf88c64598a875
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-04-26 18:19:50 +0000

    Boilerplate code set up for StocksPage and add it to routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,62 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import StockCreateForm from "./pages/stocks/StockCreateForm";
 import StockPage from "./pages/stocks/StockPage";
+import StocksPage from "./pages/stocks/StocksPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+  
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <StocksPage message="No results found. Adjust the search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <StocksPage
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/bulled"
+            render={() => (
+              <StocksPage
+                message="No results found. Adjust the search keyword or bulled a stock."
+                filter={`bulls__owner__profile=${profile_id}&ordering=-bulls__created_at&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/beared"
+            render={() => (
+              <StocksPage
+                message="No results found. Adjust the search keyword or beared a stock."
+                filter={`bears__owner__profile=${profile_id}&ordering=-bears__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/stocks/create" render={() => <StockCreateForm />} />
+          <Route
+            exact
+            path="/stocks/create"
+            render={() => <StockCreateForm />}
+          />
           <Route exact path="/stocks/:id" render={() => <StockPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
